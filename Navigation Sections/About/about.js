6195957f158d4be3a9faa8943eb114840e167e33
saomyaraj <saomyaraj.dev@gmail.com>
2024-05-19 20:08:50 +0530

    changes the styling of about section.

--- Navigation Sections/About/about.js
+++ Navigation Sections/About/about.js
@@ -1,54 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.querySelector('.menu-icon');
-    const menu = document.querySelector('.menu');
+let tl = gsap.timeline();
 
-    menuIcon.addEventListener('click', function () {
-        menu.classList.toggle('active');
+    tl.from(".about, .intro, .education, .experience, .skills, .achievements", {
+        y: 50,
+        duration: 1,
+        delay: 2.5,
+        opacity: 0,
+        stagger: 0.3
     });
-
-    let preloader = document.getElementById("preloader");
-    let dotsElement = document.getElementById("loadingDots");
-    let dotsInterval;
-
-    // Function to update dots animation
-    function updateDots() {
-        let dots = dotsElement.innerHTML;
-        dots = dots.length < 3 ? dots + '.' : '';
-        dotsElement.innerHTML = dots;
-    }
-
-    // Start updating dots every 500 milliseconds
-    dotsInterval = setInterval(updateDots, 500);
-
-    // Set a minimum duration for the preloader to be visible (in milliseconds)
-    const minimumDuration = 2000;
-
-    // Record the start time
-    let startTime = new Date().getTime();
-
-    window.addEventListener("load", function () {
-        // Calculate the elapsed time
-        let elapsedTime = new Date().getTime() - startTime;
-
-        // If less than the minimum duration, delay the hiding of the preloader
-        if (elapsedTime < minimumDuration) {
-            setTimeout(function () {
-                hidePreloader();
-            }, minimumDuration - elapsedTime);
-        } else {
-            hidePreloader();
-        }
-    });
-
-    // Function to hide the preloader
-    function hidePreloader() {
-        preloader.style.transition = "opacity 0.5s";
-        preloader.style.opacity = "0";
-
-        // Set a timeout to ensure the preloader is hidden after the transition
-        setTimeout(function () {
-            preloader.style.display = "none";
-            clearInterval(dotsInterval); // Clear the dots interval when the preloader is hidden
-        }, 500);
-    }
-});
