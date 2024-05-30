b98f1ea3b8544e098308dfb1bcc8999fed3807e0
saomyaraj <saomyaraj.dev@gmail.com>
2024-05-30 13:06:32 +0530

    added transition to navigation section.

--- Navigation Sections/About/about.js
+++ Navigation Sections/About/about.js
@@ -6,4 +6,21 @@ let tl = gsap.timeline();
         delay: 1,
         opacity: 0,
         stagger: 0.3
-    });
+});
+
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Scroll down
+        header.classList.add('header-hidden');
+    } else {
+        // Scroll up
+        header.classList.remove('header-hidden');
+    }
+
+    lastScrollTop = scrollTop;
+});
--- Navigation Sections/Blogs/blogs.js
+++ Navigation Sections/Blogs/blogs.js
@@ -7,3 +7,20 @@ tl.from(".blogs, .blog-content, .linux, .git, .genai-Ed", {
     opacity: 0,
     stagger: 0.3
 });
+
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Scroll down
+        header.classList.add('header-hidden');
+    } else {
+        // Scroll up
+        header.classList.remove('header-hidden');
+    }
+
+    lastScrollTop = scrollTop;
+});
