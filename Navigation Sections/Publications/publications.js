689cb49a51c72857c06817f4841174c2e8db1f9f
saomyaraj <saomyaraj.dev@gmail.com>
2024-11-07 10:59:24 +0530

    Changed the relative path of main files.

---
+++ Navigation Sections/Publications/publications.js
@@ -0,0 +1,26 @@
+let tl = gsap.timeline();
+
+tl.from(".heading, .blogs, .blog-content", {
+    y: 50,
+    duration: 1,
+    delay: 1,
+    opacity: 0,
+    stagger: 0.3
+});
+
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Scroll down
+        header.classList.add('header-hidden');
+    } else {
+        // Scroll up
+        header.classList.remove('header-hidden');
+    }
+
+    lastScrollTop = scrollTop;
+});
